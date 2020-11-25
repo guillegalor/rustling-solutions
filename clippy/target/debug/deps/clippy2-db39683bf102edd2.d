c905d104e07a9d5fbfb5905faff544816ab51b5b
guillegalor <guillegalor@gmail.com>
2020-11-25 11:41:24 +0100

    Añade ejercicios

---
+++ clippy/target/debug/deps/clippy2-db39683bf102edd2.d
@@ -0,0 +1,5 @@
+/home/guille/Projects/rustlings/exercises/clippy/target/debug/deps/clippy2-db39683bf102edd2.rmeta: clippy2.rs
+
+/home/guille/Projects/rustlings/exercises/clippy/target/debug/deps/clippy2-db39683bf102edd2.d: clippy2.rs
+
+clippy2.rs:
